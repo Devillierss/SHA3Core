a47b42811332eaf0b2355f77cebe46eb09e9932e
devillierss <devillierss@gmail.com>
2020-09-10 17:35:32 +0200

    Added Keccak1600

---
+++ src/SHA3KeccakCore/HashType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SHA3KeccakCore
+{
+    public enum HashType
+    {
+        Keccak = 0x01,
+        Sha3 = 0x06,
+        Shake = 0x1f,
+        CShake = 0x04
+    }
+}
