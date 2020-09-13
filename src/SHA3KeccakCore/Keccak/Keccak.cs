c86e75e1168d2e8ed4e9aac1113dae50255c1e1f
devillierss <devillierss@gmail.com>
2020-09-13 17:08:36 +0200

    chore: Changed Initialize to accept enum bitrate

--- src/SHA3KeccakCore/Keccak/Keccak.cs
+++ src/SHA3KeccakCore/Keccak/Keccak.cs
@@ -8,13 +8,9 @@ namespace SHA3KeccakCore.Keccak
 {
     public class Keccak : Keccak1600
     {
-        private readonly int _rateBytes;
-        private readonly int _keccakBits;
 
-        public Keccak(KeccakBitType bitType)
+        public Keccak(KeccakBitType bitType):base((int)bitType)
         {
-            _keccakBits = (int)bitType;
-            _rateBytes = Converters.ConvertBitLengthToRate(_keccakBits);
         }
 
         public string Hash(string stringToHash)
@@ -22,7 +18,7 @@ namespace SHA3KeccakCore.Keccak
 
             var encodedBytes = Converters.ConvertStringToBytes(stringToHash);
 
-            base.Initialize(new KeccakConfiguration() { RateBytes = _rateBytes, OutputLength = _keccakBits / 8, HashType = HashType.Keccak });
+            base.Initialize((int)HashType.Keccak);
             base.Absorb(encodedBytes, 0, encodedBytes.Length);
             base.Partial(encodedBytes, 0, encodedBytes.Length);
 
